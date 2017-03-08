1528b1aa2e17f33a6c7f359be1bb944ac56f80ac
mattie432 <matt@mattie432.com>
2017-03-08 00:28:09 +0000

    Base implementation of the Online account verifier

---
+++ Programming/3_OnlineAccountVerifier/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='OnlineAccountVerifier',
+    version='1.0.0',
+    author='Mattie432',
+    author_email='matt@mattie432.com',
+    description='',
+    url='https://github.com/Mattie432/Blockchain-Voting-System',
+    packages=[
+        'onlineaccountverifier'
+    ],
+    install_requires=[
+        'Twisted>=17.1.0'
+    ]
+)
