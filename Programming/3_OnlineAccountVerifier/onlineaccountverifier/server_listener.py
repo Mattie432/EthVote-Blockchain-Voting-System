434890a57a1a37560e4d4b91d302643e10c8d7a7
mattie432 <matt@mattie432.com>
2017-03-10 03:30:08 +0000

    Refactor modules

--- Programming/3_OnlineAccountVerifier/onlineaccountverifier/server_listener.py
+++ Programming/3_OnlineAccountVerifier/onlineaccountverifier/server_listener.py
@@ -6,7 +6,7 @@ class ServerListener():
 
     def __init__(self):
         print ("Init")
-        # Import postgres & twisted credentials from environment. Throws if dont exist.
+        # Import postgres & twisted credentials from environment. Throws if does not exist.
         self.postgres_database   = os.environ[ 'POSTGRES_DATABASE' ]
         self.postgres_user       = os.environ[ 'POSTGRES_USER' ]
         self.postgres_password   = os.environ[ 'POSTGRES_PASS' ]
@@ -14,15 +14,17 @@ class ServerListener():
         self.twisted_port        = int(os.environ[ 'TWISTED_PORT' ])
 
 
-        print ("[main] Connecting to database\n"
+        print ("[ServerListener] Connecting to database\n"
            "    -> database:'%s' user:'%s' host:'%s'" % (self.postgres_database, self.postgres_user, self.postgres_host) )
 
         # get a connection, if a connect cannot be made an exception will be raised here
         self.connection = psycopg2.connect(database=self.postgres_database, user=self.postgres_user, host=self.postgres_host, password=self.postgres_password)
         self.cursor = self.connection.cursor()
 
+        print("[ServerListener] Database connection sucsessful")
+
     def database_shutdown(self):
-        print ("[main] Shutting down database connection.")
+        print ("[ServerListener] Shutting down database connection.")
         self.cursor.close()
         self.connection.close()
 
@@ -35,15 +37,15 @@ class ServerListener():
     class Listener(protocol.Protocol):
 
         def __init__(self):
-            print("[Listener] Connection established")
+            print("[ServerListener - Listener] Connection established")
 
         def connectionMade(self):
-            print("[Listener] Connection made")
+            print("[ServerListener - Listener] Connection made")
 
         def connectionLost(self, reason="connectionDone"):
-            print("[Listener] Connection Lost : %s" % reason)
+            print("[ServerListener - Listener] Connection Lost : %s" % reason)
 
         def dataReceived(self, data):
             "As soon as any data is received, write it back."
-            print( "[Listener] Data received '%s'" % data )
+            print( "[ServerListener - Listener] Data received '%s'" % data )
             self.transport.write(data)
